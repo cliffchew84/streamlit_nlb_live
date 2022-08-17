b526a36f3aabaa624c3f777afe5d39123130a829
kelifuzhou <cliffchew84@gmail.com>
2022-08-18 02:32:06 +0800

    update requirements.txt

--- Main_Page.py
+++ Main_Page.py
@@ -1,6 +1,24 @@
 # Contents of ~/my_app/main_page.py
 import streamlit as st
 
+def main_page():
+    st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+    st.sidebar.markdown("# Step 1 Get Yr Bks")
+
+def page3():
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Step 1 Get Yr Bks": page2,
+    "Page 3": page3,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
+
 st.markdown("""
 ### NLB Bookmarked Books Aggregator!
 #### Why this web app?
