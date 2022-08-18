ae5fd1ac3fb1f39f06225cbd73473fc65f0f5a47
kelifuzhou <cliffchew84@gmail.com>
2022-08-18 12:31:54 +0800

    modify selenium tag

--- pages/Step_4_test_selenium_nlb.py
+++ pages/Step_4_test_selenium_nlb.py
@@ -87,14 +87,14 @@ except:
 
 for i in range(1,counter-2):
     try:
-        browser.find_element(By.CSS_SELECTOR, '#bookmark-folder-content > nav > ul > li:nth-child(5) > a').click()
+        driver.find_element(By.CSS_SELECTOR, '#bookmark-folder-content > nav > ul > li:nth-child(5) > a').click()
     except:
         print(f'{i} error')
     
     next_button = '//*[@id="bookmark-folder-content"]/nav/ul/li[5]/a'
-    browser.find_element('xpath', next_button).click()
+    driver.find_element('xpath', next_button).click()
     
-    soup = bs(browser.page_source, 'html5lib')
+    soup = bs(driver.page_source, 'html5lib')
     book_urls_dict[i] = list(set(get_book_urls_on_page(soup)))
     time.sleep(2)
 
