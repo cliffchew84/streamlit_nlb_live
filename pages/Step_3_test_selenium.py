86c9cd3650a593c7e8ddfb26309a2c58713429d6
kelifuzhou <cliffchew84@gmail.com>
2022-08-18 10:56:17 +0800

    test selenium online eg

--- pages/Step_3_test_selenium.py
+++ pages/Step_3_test_selenium.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 options = Options()
 options.add_argument("--headless")
