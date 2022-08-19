0514b377a9f56f7d536bfa41d6d3b4b2748820f5
kelifuzhou <cliffchew84@gmail.com>
2022-08-19 15:39:34 +0800

    clean up selenium test

--- pages/Step_4_test_selenium_nlb.py
+++ pages/Step_4_test_selenium_nlb.py
@@ -12,6 +12,7 @@ from bs4 import BeautifulSoup as bs
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from nlb_api_fun import *
 
