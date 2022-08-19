6f510d5fbad6a8cf282e1fd3c56a6ef8330da105
kelifuzhou <cliffchew84@gmail.com>
2022-08-19 16:13:38 +0800

    final clean up of pages

--- pages/Step_1_Get_Yr_Bks.py
+++ pages/Step_1_Get_Yr_Bks.py
@@ -14,23 +14,65 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from zAbout_Me import *
 
-API = st.secrets['nlb_api_keys']
-account_name = st.secrets['nlb_login_account']
-password = st.secrets['nlb_login_pw']
+PRODUCTION_URL = "https://openweb.nlb.gov.sg/OWS/CatalogueService.svc?singleWsdl"
+client = Client(wsdl=PRODUCTION_URL)
 
-# Web scraping parameters
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-features=NetworkService")
-options.add_argument("--window-size=1920x1080")
-options.add_argument("--disable-features=VizDisplayCompositor")
-options.add_argument('''--user-agent="Mozilla/5.0 (Windows NT 6.1; 
-WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"''')
+#### Functions needed
+def return_needed_url(id_: str) -> str:
+    return f"https://eservice.nlb.gov.sg/item_holding.aspx?id={id_}&type=bid&app=mylibrary"
+
+
+def make_get_avail_api_call(API: str, bid_no: str, client=client):
+    """ 
+    Takes in book NLB BID number and
+    returns GetAvailabilityInfo Obj
+    """
+    get_avail = {
+        "APIKey": API,
+        "BID": bid_no,
+        "Modifiers" : {
+            "SortSchema": None,
+            "StartRecordPosition": 1,
+            "MaximumRecords": 10,
+            "SetId": None
+        },
+    }
+
+    return client.service.GetAvailabilityInfo(**get_avail)
+
+
+def df_get_avail_data(bid_no: str, avail_info_obj):
+    
+    return pd.DataFrame(
+        helpers.serialize_object(avail_info_obj).get("Items").get("Item")
+    ).sort_values("BranchName")
+
+
+def make_get_title_details_api_call(API: str, bid_no: str, client=client):
+    
+    title_inputs = { "APIKey": API, "BID": bid_no }
+    return client.service.GetTitleDetails(**title_inputs)
+
+
+def df_get_title_data(title_detail_obj):
+    
+    return pd.DataFrame(
+        helpers.serialize_object(title_detail_obj).get("TitleDetail"))
+
+
+def final_book_avail_df(
+    avail_df: pd.DataFrame, 
+    title_df: pd.DataFrame
+    ) -> pd.DataFrame:
+    
+    """ Combining book available data and title data """
+    book_output = avail_df[
+        ['BranchName', 'CallNumber', 'StatusCode', 
+        'StatusDesc', 'StatusDate', 'DueDate']]
+    
+    book_output['TitleName'] = title_df.TitleName.values[0]
+    return book_output
 
 
 def log_in_nlb(driver, account_name: str, password: str):
@@ -65,6 +107,24 @@ def get_book_urls_on_page(soup):
         if "catalogue" in a['href']:
             book_urls_list.append(a['href'])
     return book_urls_list
+
+#### Parameters
+API = st.secrets['nlb_api_keys']
+account_name = st.secrets['nlb_login_account']
+password = st.secrets['nlb_login_pw']
+
+# Web scraping parameters
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-features=NetworkService")
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-features=VizDisplayCompositor")
+options.add_argument('''--user-agent="Mozilla/5.0 (Windows NT 6.1; 
+WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"''')
+
 
 ############ Actual code ############
 
