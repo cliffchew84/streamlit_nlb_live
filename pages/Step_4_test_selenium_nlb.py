61defe2a572e251db77b831e738dec9558f2e2e0
kelifuzhou <cliffchew84@gmail.com>
2022-08-19 15:37:34 +0800

    clean up selenium test

--- pages/Step_4_test_selenium_nlb.py
+++ pages/Step_4_test_selenium_nlb.py
@@ -13,18 +13,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 
-from nlb_api_fun import (
-    log_in_nlb,
-    get_book_urls_on_page,
-    return_needed_url,
-    make_get_avail_api_call,
-    df_get_avail_data,
-    make_get_title_details_api_call,
-    df_get_title_data,
-    final_book_avail_df
-)
+from nlb_api_fun import *
 
-# Streamlit secr
 API = st.secrets['nlb_api_keys']
 account_name = st.secrets['nlb_login_account']
 password = st.secrets['nlb_login_pw']
@@ -41,12 +31,44 @@ options.add_argument("--disable-features=VizDisplayCompositor")
 options.add_argument('''--user-agent="Mozilla/5.0 (Windows NT 6.1; 
 WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"''')
 
-## Login in NLB
+
+def log_in_nlb(driver, account_name: str, password: str):
+    """ Logins into the NLB app, and returns selenium driver object
+    """
+
+    # Go login page
+    driver.get('https://cassamv2.nlb.gov.sg/cas/login')
+    time.sleep(1)
+    
+    account_info = [account_name, password]
+    tag_info = ["""//*[@id="username"]""", """//*[@id="password"]"""]
+    
+    for info, tag in zip(account_info, tag_info):
+        driver.find_element("xpath", tag).send_keys("{}".format(info))
+        time.sleep(1)
+    
+    # Click login
+    driver.find_element("xpath", """//*[@id="fm1"]/section/input[4]""").click()
+    return driver
+
+
+def get_book_urls_on_page(soup):
+    """ Getting book urls from page for NLB project 
+    Args: BeautifulSoup Object
+    Returns: List of book urls (list)
+    
+    """
+
+    book_urls_list = list()
+    for a in soup.find_all('a', href=True):
+        if "catalogue" in a['href']:
+            book_urls_list.append(a['href'])
+    return book_urls_list
+
+## Apply scraping
 driver = webdriver.Chrome(options=options)
 log_in_nlb(driver, account_name, password)
-st.markdown("Login in into your NLB account")
 
-## Go to bookmarks
 url_link = "https://www.nlb.gov.sg/mylibrary/Bookmarks"
 driver.get(url_link)
 time.sleep(5)
@@ -85,7 +107,6 @@ for i in range(1,counter-2):
         book_urls_dict[i] = list(set(get_book_urls_on_page(soup)))
 
 # Produce out all my urls
-st.write("These are all your bookmarked books from NLB")
 st.write(book_urls_dict)
 
 all_book_url_lists = list()
@@ -104,22 +125,22 @@ bid_w_issues = list()
 my_bar = st.progress(0)
 
 for i, bid_no in enumerate(list_of_book_bids):
-    try:
-        avail_book_obj = make_get_avail_api_call(API, bid_no)
-        avail_book_df = df_get_avail_data(bid_no, avail_book_obj)
+    # try:
+    avail_book_obj = make_get_avail_api_call(API, bid_no)
+    avail_book_df = df_get_avail_data(bid_no, avail_book_obj)
 
-        title_detail_obj = make_get_title_details_api_call(bid_no)
-        title_detail_df = df_get_title_data(title_detail_obj)
-        
-        final_book_df = final_book_avail_df(avail_book_df, title_detail_df)
-        final_book_df['url'] = return_needed_url(API, bid_no)
-        
-        df = df.append(final_book_df)
-        my_bar.progress(i/max_books)
+    title_detail_obj = make_get_title_details_api_call(bid_no)
+    title_detail_df = df_get_title_data(title_detail_obj)
     
-    except:
-        bid_w_issues.append(bid_no)
-        my_bar.progress(i/max_books)
+    final_book_df = final_book_avail_df(avail_book_df, title_detail_df)
+    final_book_df['url'] = return_needed_url(API, bid_no)
+    
+    df = df.append(final_book_df)
+    my_bar.progress(i/max_books)
+    
+    # except:
+    #     bid_w_issues.append(bid_no)
+    #     my_bar.progress(i/max_books)
 
 df = df.to_csv(index=False).encode('utf-8')
 
