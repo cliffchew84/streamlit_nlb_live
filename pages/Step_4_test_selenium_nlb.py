0869d208a426675ae477cd56fb876780e7e0f313
kelifuzhou <cliffchew84@gmail.com>
2022-08-19 15:28:21 +0800

    clean up selenium test

--- pages/Step_4_test_selenium_nlb.py
+++ pages/Step_4_test_selenium_nlb.py
@@ -1,8 +1,7 @@
 import os
 import re
-import time
 import math
-import sys
+import time
 import numpy as np
 import pandas as pd
 import streamlit as st
@@ -13,21 +12,24 @@ from bs4 import BeautifulSoup as bs
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
+from nlb_api_fun import (
+    log_in_nlb,
+    get_book_urls_on_page,
+    return_needed_url,
+    make_get_avail_api_call,
+    df_get_avail_data,
+    make_get_title_details_api_call,
+    df_get_title_data,
+    final_book_avail_df
+)
 
-from nlb_api_fun import *
-
-if sys.platform == "darwin":
-    API = os.environ['nlb_api_keys']
-
-else:
-    API = st.secrets['nlb_api_keys']
-
+# Streamlit secr
+API = st.secrets['nlb_api_keys']
 account_name = st.secrets['nlb_login_account']
 password = st.secrets['nlb_login_pw']
 
-
+# Web scraping parameters
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
@@ -39,44 +41,12 @@ options.add_argument("--disable-features=VizDisplayCompositor")
 options.add_argument('''--user-agent="Mozilla/5.0 (Windows NT 6.1; 
 WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"''')
 
-
-def log_in_nlb(driver, account_name: str, password: str):
-    """ Logins into the NLB app, and returns selenium driver object
-    """
-
-    # Go login page
-    driver.get('https://cassamv2.nlb.gov.sg/cas/login')
-    time.sleep(1)
-    
-    account_info = [account_name, password]
-    tag_info = ["""//*[@id="username"]""", """//*[@id="password"]"""]
-    
-    for info, tag in zip(account_info, tag_info):
-        driver.find_element("xpath", tag).send_keys("{}".format(info))
-        time.sleep(1)
-    
-    # Click login
-    driver.find_element("xpath", """//*[@id="fm1"]/section/input[4]""").click()
-    return driver
-
-
-def get_book_urls_on_page(soup):
-    """ Getting book urls from page for NLB project 
-    Args: BeautifulSoup Object
-    Returns: List of book urls (list)
-    
-    """
-
-    book_urls_list = list()
-    for a in soup.find_all('a', href=True):
-        if "catalogue" in a['href']:
-            book_urls_list.append(a['href'])
-    return book_urls_list
-
-## Apply scraping
+## Login in NLB
 driver = webdriver.Chrome(options=options)
 log_in_nlb(driver, account_name, password)
+st.markdown("Login in into your NLB account")
 
+## Go to bookmarks
 url_link = "https://www.nlb.gov.sg/mylibrary/Bookmarks"
 driver.get(url_link)
 time.sleep(5)
@@ -115,6 +85,7 @@ for i in range(1,counter-2):
         book_urls_dict[i] = list(set(get_book_urls_on_page(soup)))
 
 # Produce out all my urls
+st.write("These are all your bookmarked books from NLB")
 st.write(book_urls_dict)
 
 all_book_url_lists = list()
@@ -133,22 +104,22 @@ bid_w_issues = list()
 my_bar = st.progress(0)
 
 for i, bid_no in enumerate(list_of_book_bids):
-    # try:
-    avail_book_obj = make_get_avail_api_call(API, bid_no)
-    avail_book_df = df_get_avail_data(bid_no, avail_book_obj)
+    try:
+        avail_book_obj = make_get_avail_api_call(API, bid_no)
+        avail_book_df = df_get_avail_data(bid_no, avail_book_obj)
 
-    title_detail_obj = make_get_title_details_api_call(bid_no)
-    title_detail_df = df_get_title_data(title_detail_obj)
+        title_detail_obj = make_get_title_details_api_call(bid_no)
+        title_detail_df = df_get_title_data(title_detail_obj)
+        
+        final_book_df = final_book_avail_df(avail_book_df, title_detail_df)
+        final_book_df['url'] = return_needed_url(API, bid_no)
+        
+        df = df.append(final_book_df)
+        my_bar.progress(i/max_books)
     
-    final_book_df = final_book_avail_df(avail_book_df, title_detail_df)
-    final_book_df['url'] = return_needed_url(API, bid_no)
-    
-    df = df.append(final_book_df)
-    my_bar.progress(i/max_books)
-    
-    # except:
-    #     bid_w_issues.append(bid_no)
-    #     my_bar.progress(i/max_books)
+    except:
+        bid_w_issues.append(bid_no)
+        my_bar.progress(i/max_books)
 
 df = df.to_csv(index=False).encode('utf-8')
 
